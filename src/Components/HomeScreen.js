1ee419a5d5e4b7f18f721d5cf106f809ccc75dcf
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2021-12-27 00:46:27 -0300

    Yarn add react-native-reanimated
    Animated.View y todo sigue funcionando

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -11,16 +11,36 @@ import {
     Container,
     CheckBox,
     Body,
-    Right
+    Right,
+    Center
 } from 'native-base'
-
+import Animated from 'react-native-reanimated'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Homee Screen</Text>
-        </View>
+        <Animated.View style={{
+            flex:1,
+            alignItems: 'center',
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+        }}>
+        <LinearGradient 
+        colors={['#FAAD3D', '#EFC90A', '#f1cb0c']}
+        style={{...styles.Box}}
+        >
+        </LinearGradient>
+        </Animated.View>
     )
 }
+
+const styles = StyleSheet.create({
+    Box:{
+        width: '100%',
+        height: 189,
+        borderRadius:15,
+        flexDirection: 'row',
+        padding: 22
+    }
+})
 
 export default HomeScreen
